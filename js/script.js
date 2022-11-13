3cace81fd4a90f48cb2a5bbd53f8399c68617e19
Sailor-Moon-Moon <risa.clar123@mail.ru>
2022-11-13 13:11:12 +0300

    a

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
+
 function burgerMenu(selector) {
     let menu = $(selector);
     let button = menu.find('.burger-menu__button');
-    let links = menu.find('.burger-menu__link');
-    let overlay = menu.find('.burger-menu__overlay');
+    let links = menu.find('.burger-menu__link1','.burger-menu__link2','.burger-menu__link3');
     
     button.on('click', (e) => {
         e.preventDefault();
         toggleMenu();
     });
     
-    links.on('click', () => toggleMenu());
-    overlay.on('click', () => toggleMenu());
     
     function toggleMenu() {
         menu.toggleClass('burger-menu_active');
-        if (menu.hasClass('burger-menu_active')) {
-            $('body').css('overflow', 'hidden');
-        } else {
-            $('body').css('overflow', 'visible');
-        }
+        
     }
 }
 
