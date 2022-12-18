920a327469d35ed20dfc86cdc74d5f36370f46ef
Squora <87583694+Squora@users.noreply.github.com>
2022-12-18 14:10:14 +0300

    Add files via upload

---
+++ squora.github.io-main/js/mapSwitcher.js
@@ -0,0 +1,10 @@
+function showElementsBS_01() 
+{
+    document.getElementById("bsElements01").style.display = "block";
+}
+
+function showElementsBS_02() 
+{
+    document.getElementById("bsElements01").style.display = "none";
+    document.getElementById("bsElements02").style.display = "block";
+}
