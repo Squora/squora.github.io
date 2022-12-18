48085cd8b3b38ac911a2d068ff7f4778d971ce72
Sailor-Moon-Moon <risa.clar123@mail.ru>
2022-12-18 18:38:53 +0300

    lalalala

---
+++ js/mapSwitcher.js
@@ -0,0 +1,105 @@
+
+function showElementsBS_01() 
+{
+    document.getElementById("bsElements01").style.display = "block";
+    document.getElementById("bsElements02").style.display = "none";
+    document.getElementById("bsElements03").style.display = "none";
+    document.getElementById("bsElements04").style.display = "none";
+}
+
+function showbsMagnifierСС() 
+{
+    document.getElementById("bsMagnifierСС").style.display = "block";
+
+}
+
+function showbsMagnifierСС2() 
+{
+    document.getElementById("bsMagnifierСС").style.display = "none";
+    document.getElementById("bsMagnifier2СС").style.display = "block";
+}
+
+function showbsMagnifierСС_OF() 
+{
+    document.getElementById("bsMagnifier2СС").style.display = "none";
+}
+
+
+
+function showElementsBS_02() 
+{
+    document.getElementById("bsElements01").style.display = "none";
+    document.getElementById("bsElements02").style.display = "block";
+    document.getElementById("bsElements03").style.display = "none";
+    document.getElementById("bsElements04").style.display = "none";
+}
+
+
+
+
+function showElementsBS_03() 
+{
+    document.getElementById("bsElements01").style.display = "none";
+    document.getElementById("bsElements02").style.display = "none";
+    document.getElementById("bsElements03").style.display = "block";
+    document.getElementById("bsElements04").style.display = "none";
+}
+
+function showbsMagnifierPH() 
+{
+    document.getElementById("bsMagnifierPH").style.display = "block";
+
+}
+
+function showbsMagnifierPH2() 
+{
+    document.getElementById("bsMagnifierPH").style.display = "none";
+    document.getElementById("bsMagnifier2PH").style.display = "block";
+}
+
+function showbsMagnifierPH_OF() 
+{
+    document.getElementById("bsMagnifier2PH").style.display = "none";
+}
+
+
+function showElementsBS_04() 
+{
+    document.getElementById("bsElements01").style.display = "none";
+    document.getElementById("bsElements02").style.display = "none";
+    document.getElementById("bsElements03").style.display = "none";
+    document.getElementById("bsElements04").style.display = "block";
+}
+
+function showbsMagnifierMC() 
+{
+    document.getElementById("bsMagnifierMC").style.display = "block";
+
+}
+
+function showbsMagnifierMC2() 
+{
+    document.getElementById("bsMagnifierMC").style.display = "none";
+    document.getElementById("bsMagnifier2MC").style.display = "block";
+}
+
+function showbsMagnifierMC_OF() 
+{
+    document.getElementById("bsMagnifier2MC").style.display = "none";
+}
+function showbsMagnifierCL() 
+{
+    document.getElementById("bsMagnifierCL").style.display = "block";
+
+}
+
+function showbsMagnifierCL2() 
+{
+    document.getElementById("bsMagnifierCL").style.display = "none";
+    document.getElementById("bsMagnifier2CL").style.display = "block";
+}
+
+function showbsMagnifierCL_OF() 
+{
+    document.getElementById("bsMagnifier2CL").style.display = "none";
+}
